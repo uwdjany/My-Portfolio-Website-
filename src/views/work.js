ac335e0d33dbd4e71dd4dabc6c7564e6249723d5
uwdjany <djanatiuwase@gmail.com>
2022-10-01 22:27:49 +0200

    some changes added in skills page

--- src/views/work.js
+++ src/views/work.js
@@ -10,53 +10,7 @@ const Work = () => {
           <h2>My Work</h2>
           
           </div>
-          <div classNameName="row">
-            <div classNameName="col-lg-4">
-              <div>
-                <img src="..." className="card-img-top" alt="..." />
-                <div>
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div classNameName="col-lg-4">
-            <div>
-              <img src="..." className="card-img-top" alt="..." />
-              <div>
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make
-                  up the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-          </div>
-          <div classNameName="col-lg-4">
-          <div>
-            <img src="..." className="card-img-top" alt="..." />
-            <div>
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make
-                up the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-          </div>
+         
         </div>
       </section>
     </>
