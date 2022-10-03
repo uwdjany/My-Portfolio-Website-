3dd9c8bac332ee13284e0a7f00192ceba73437ac
uwdjany <djanatiuwase@gmail.com>
2022-10-03 21:31:34 +0200

    some changes Added

---
+++ src/assets/workData/myData.js
@@ -0,0 +1,37 @@
+export const myWorkData =[
+    {
+        id:1,
+        title:'Rwanda booking Tour',
+        price:'$666',
+        category:"it easy to book ticket",
+        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
+    },
+    {
+        id:2,
+        title:'E-Learning Material',
+        price:'$666',
+        category:"it easy to book ticket",
+        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
+    },
+    {
+        id:3,
+        title:'Rwanda booking Tour',
+        price:'$666',
+        category:"it easy to book ticket",
+        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
+    },
+    {
+        id:1,
+        title:'Rwanda booking Tour',
+        price:'$666',
+        category:"it easy to book ticket",
+        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
+    },
+    {
+        id:1,
+        title:'Rwanda booking Tour',
+        price:'$666',
+        category:"it easy to book ticket",
+        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
+    },
+]
--- src/views/work.js
+++ src/views/work.js
@@ -1,16 +1,35 @@
 import React from "react";
 import "./workstyle.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
+import { myWorkData } from "../assets/workData/myData";
 const Work = () => {
   return (
     <>
       <section id="work">
-        <div classNameName="container">
-          <div className="text-center pt-5">
-          <h2>My Work</h2>
-          
-          </div>
-         
+
+
+    
+      <div className="container">
+      { myWorkData.map(item=>(
+        <div className="card">
+        <div className="card-top" id="card-one">
+        <img src={item.linkImg} alt={item.title} id="img-work"/>
+        <h1>{item.title}</h1>
+        
+        </div>
+        <div className="card-bottom">
+        <h3>{item.price}</h3>
+        <p>{item.category}</p>
+        </div>
+        </div>
+
+
+      ))}
+       
+       
         </div>
       </section>
     </>
