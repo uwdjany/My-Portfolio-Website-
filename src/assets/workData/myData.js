28f498f559bc20aafa5e843835c810753d971e34
uwdjany <djanatiuwase@gmail.com>
2022-10-14 11:35:17 +0200

    slide card added

--- src/assets/workData/myData.js
+++ src/assets/workData/myData.js
@@ -1,37 +1,22 @@
 export const myWorkData =[
     {
         id:1,
+        image:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg',
         title:'Rwanda booking Tour',
-        price:'$666',
-        category:"it easy to book ticket",
-        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
     },
     {
         id:2,
-        title:'E-Learning Material',
-        price:'$666',
-        category:"it easy to book ticket",
-        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
+        image:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg',
+        title:'Rwanda booking Tour',
     },
     {
         id:3,
+        image:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg',
         title:'Rwanda booking Tour',
-        price:'$666',
-        category:"it easy to book ticket",
-        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
     },
     {
-        id:1,
+        id:4,
+        image:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg',
         title:'Rwanda booking Tour',
-        price:'$666',
-        category:"it easy to book ticket",
-        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
-    },
-    {
-        id:1,
-        title:'Rwanda booking Tour',
-        price:'$666',
-        category:"it easy to book ticket",
-        linkImg:'https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg'
     },
 ]
